1a650c2a6ccac555dc4992bd9547b68a0dc46b84
kojii <saito@grw.sc>
2013-01-22 20:28:55 +0900

    MongoLabを参照するようにした。
    MongoDBのポート番号を明示的に指定
    
    MongoLabを参照するように変更

--- models/channel.js
+++ models/channel.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose')
   , Timestamps = require('./timestamps')
   , Extends = require('./extends')
   , Schema = mongoose.Schema
-  , db = mongoose.connect("mongodb://localhost/radihey");
+  , db = mongoose.connect("mongodb://heroku_app10506621:rg03r91cg78i0qi9j88rrnnujq@ds037817.mongolab.com:37817/heroku_app10506621");
 
 var Channel = new Schema({
     title: { type: String, required: true },
