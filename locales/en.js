5e3b790af0f93f6c3e2e3b186df4f11f434aec18
johnny <j.e.miyake@gmail.com>
2013-01-24 15:07:04 +0900

    少し国際化対応した

--- locales/en.js
+++ locales/en.js
@@ -1,7 +1,11 @@
 {
-	"nav.title": "nav.title",
-	"title": "title",
-	"channel": "channel",
-	"edit": "edit",
-	"service-description": "service-description"
+  "title": "Radihey",
+  "channel": "Channels",
+  "new": "New Channel",
+  "edit": "Edit Channel",
+  "service-description": "is a system that you can share your impressions of the radio program you are listening.",
+  "channels-available": "Channels available on Radihey",
+  "new-channel": "New Channel",
+  "listening": " people are listening this program!",
+  "currently": "Currently, "
 }
